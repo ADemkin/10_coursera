334cbeb6c0bfcfb366ce30290967f7aba523bf83
antondemkin <antondemkin@yandex.ru>
2017-08-07 09:31:48 +0300

    Firtst working commit.

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+This script will get random 50 courses from [Coursera xml feed](https://www.coursera.org/sitemap~www~courses.xml), then parse some data directly from coursera webpage using BeautifulSoup4 and, finally, will create .xlsx file with parsed data.
+
+Anton Demkin, 2017
+
+# Installation
+Script requires Python 3. To install additional modules run this terminal command from folder with this script:
+```
+pip3 install -r requirements.txt
+```
+
+# How to use
+
+Simply run this script with output excel file as single argument. Script will automatically do the rest. Excel file will be created in same folder with this script.
+```
+python3 coursera.py coursera.xlsx 
+```
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,18 +1,10 @@
-from lxml import etree
-from io import StringIO, BytesIO
-
-from xml.etree import ElementTree as et
-
+from xml.etree import ElementTree
 from bs4 import BeautifulSoup
-
-import openpyxl
+from openpyxl import Workbook
 import requests
-
 import random
-
-import collections
-
 import re
+import sys
 
 coursera_xml_url = 'https://www.coursera.org/sitemap~www~courses.xml'
 
@@ -23,72 +15,67 @@ def get_courses_list():
     except requests.exceptions.ConnectionError as error:
         print(error)
     else:
-        et_data = et.fromstring(data)
-        return [element[0].text for element in et_data]
+        element_tree_data = ElementTree.fromstring(data)
+        return [element[0].text for element in element_tree_data]
 
 
 def get_course_info(course_url):
-    # TODO: get name, lang, nearest start, learning weeks, avg score
-    # TODO: for course in courses_url
-    course = course_url
     try:
-        course_html = requests.get(course).content
+        course_html = requests.get(course_url).content
     except requests.exceptions.ConnectionError as error:
         print(error)
     else:
-        # print(course_html)
         soup = BeautifulSoup(course_html, 'lxml')
-        print("Url: {}".format(course))
         
-        name = soup.find("h1").string  # working!
-        print("Name: {}".format(name))
-        # language = soup.find_all(attrs={'data-reactid':'165'})[0].string
+        name = soup.find("h1").string
         language = soup.find('div', class_='language-info').contents[0].contents[1]
         
-        print("Lang: {}".format(language))
-        # commitment = soup.find_all(attrs={'data-reactid':158})[0].string  # working!
-        
-        # commitment is not always present
         try:
             commitment = soup.find('span', class_='td-title', string='Commitment').parent.parent.contents[1].string
         except AttributeError:
             commitment = 'No info'
-            
-        print("Comm: {}".format(commitment))
         
-        # avg_rating = soup.find(string=re.compile('(\d(\.\d)?(?=\sout\sof\s5\sof\s))'))
-        #avg_rating = soup.find('div', class_='ratings-text headline-2-text').contents[0].contents[1]
-        
-        # some courses have no ratings
         try:
             avg_rating = re.sub('(Rated\s)|(\sout\sof\s5\sof\s)',
                                 '',
                                 soup.find('div', class_='ratings-text headline-2-text').contents[0].contents[1])
         except AttributeError:
-            avg_rating = 'No rating for this course'
-            
-        print("Avg rating: {}".format(avg_rating))
+            avg_rating = 'No rating'
         
-
         nearest_start = re.sub("Starts\s",
                                '',
-                               soup.find("div",
-                                         class_='startdate rc-StartDateString caption-text').contents[0].string)
-        print("Start: {}".format(nearest_start))
-        print("")
+                               soup.find("div", class_='startdate rc-StartDateString caption-text').contents[0].string)
+        
+        print(".", end="")
+        return name, language, commitment, avg_rating, nearest_start
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(filedata, filepath):
+    work_book = Workbook()
+    excel_table = work_book.active
+    
+    for row in filedata:
+        excel_table.append(row)
+    
+    work_book.save(filepath)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: coursera.py [filename]")
+        exit()
+    
+    filename = sys.argv[1]
+    courses_urls = get_courses_list()
+    random.shuffle(courses_urls)
+    courses_data = []
+    print("Working... may take some time.")
+    max_courses = 50
+    for url in courses_urls[:max_courses]:
+        courses_data.append(get_course_info(url))
+    
+    output_courses_info_to_xlsx(courses_data, filepath=filename)
 
 
 if __name__ == '__main__':
-    courses_urls = get_courses_list()
-    print("Total courses: {}".format(len(courses_urls)))
-    random.shuffle(courses_urls)  # TODO: maybe remove this
-    # print(courses_urls)
-    # courses_urls = ['https://www.coursera.org/learn/infrastructure-investing']
-    # courses_urls = ['https://www.coursera.org/specializations/excel-mysql']
-    for url in enumerate(courses_urls):
-        print(url[0])
-        get_course_info(url[1])
+    main()
