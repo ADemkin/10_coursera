b94b7a659ed5102fd92e11884e4daadf5a4e2de3
antondemkin <antondemkin@yandex.ru>
2017-08-13 07:22:20 +0300

    Url loading were carried out ot separate function from html parse.

--- coursera.py
+++ coursera.py
@@ -18,35 +18,38 @@ def get_courses_list():
         element_tree_data = ElementTree.fromstring(courses_data)
         return [element[0].text for element in element_tree_data]
 
-
-def get_course_info(course_url):
+def load_html_from_url(url):
     try:
-        course_html = requests.get(course_url).content
+        raw_html = requests.get(url).content
     except requests.exceptions.ConnectionError as error:
         print(error)
     else:
-        soup = BeautifulSoup(course_html, 'lxml')
-        
-        title = soup.find("h1").string
-        language = soup.find('div', class_='language-info').contents[0].contents[1]
-        
-        try:
-            commitment = soup.find('span', class_='td-title', string='Commitment').parent.parent.contents[1].string
-        except AttributeError:
-            commitment = 'No info'
-        
-        try:
-            rating_soup = soup.find('div', class_='ratings-text headline-2-text').contents[0].contents[1]
-        except AttributeError:
-            rating = 'No rating'
-        else:
-            # filter out anything but pure rating, maybe use as digit in future
-            rating = re.sub('(Rated\s)|(\sout\sof\s5\sof\s)', '', rating_soup)
-        
-        start_soup = soup.find("div", class_='startdate rc-StartDateString caption-text').contents[0].string
-        start = re.sub("Starts\s", '', start_soup)
-        
-        return title, language, commitment, rating, start
+        return raw_html
+
+def get_course_info(course_html):
+
+    soup = BeautifulSoup(course_html, 'lxml')
+    
+    title = soup.find("h1").string
+    language = soup.find('div', class_='language-info').contents[0].contents[1]
+    
+    try:
+        commitment = soup.find('span', class_='td-title', string='Commitment').parent.parent.contents[1].string
+    except AttributeError:
+        commitment = 'No info'
+    
+    try:
+        rating_soup = soup.find('div', class_='ratings-text headline-2-text').contents[0].contents[1]
+    except AttributeError:
+        rating = 'No rating'
+    else:
+        # filter out anything but pure rating, maybe use as digit in future
+        rating = re.sub('(Rated\s)|(\sout\sof\s5\sof\s)', '', rating_soup)
+    
+    start_soup = soup.find("div", class_='startdate rc-StartDateString caption-text').contents[0].string
+    start = re.sub("Starts\s", '', start_soup)
+    
+    return title, language, commitment, rating, start
 
 
 def output_courses_info_to_xlsx(filedata, filepath):
@@ -69,9 +72,9 @@ def main():
     random.shuffle(courses_urls)
     courses_data = []
     print("Working... may take some time.")
-    max_courses = 20
+    max_courses = 3
     for url in courses_urls[:max_courses]:
-        courses_data.append(get_course_info(url))
+        courses_data.append(get_course_info(load_html_from_url(url)))
     
     output_courses_info_to_xlsx(courses_data, filepath=filename)
     print('Done! File {} created.'.format(filename))
